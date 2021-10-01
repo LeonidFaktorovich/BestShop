e6d4fbd377ca59cdbefb4ad13ec9e61ef6aeff78
LeonidFaktorovich <lefaktorovich@edu.hse.ru>
2021-10-01 10:15:07 +0300

    project

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
+from lib.window import ShopWindow
 if __name__ == '__main__':
     app = QApplication([])
     window = ShopWindow()
     window.show()
     app.exec()
-   
--- lib/window.py
+++ lib/window.py
@@ -1,3 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+
+
 class ShopWindow(QMainWindow):
-    def
+    def __init__(self):
+        super().__init__()
+
+        self.label = QLabel('Бронирование товаров в магазине', self)
+        self.label.setGeometry(10, -100, 300, 300)
+        self.label.setText('другой текст')
+        self.button = QPushButton('')
+        self.resize(1000, 300)
